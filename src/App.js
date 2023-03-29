c53f85445bb53c6ef05bfb8fa6115c231aa9c6c2
catkidd <catkidd770@gmail.com>
2023-03-30 01:48:46 +0545

    feat: login form created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import  Login  from './components/LoginPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>hello </p>
+      <Login />
     </div>
   );
 }
