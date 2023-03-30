3286838f96679651c149235a4b1dfa0980c8ab6f
catkidd <catkidd770@gmail.com>
2023-03-31 02:00:52 +0545

    fix: minor fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import  Login  from './components/LoginPage';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ForgetPass from "./components/ForgetPass";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import { Homepage } from "./pages/homepage";
+import ProtectedRoutes from "./services/ProtectedRoutes";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<ProtectedRoutes />}>
+                    <Route path="/" element={<Homepage />} />
+                </Route>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/forget-password" element={<ForgetPass />} />
+                <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
